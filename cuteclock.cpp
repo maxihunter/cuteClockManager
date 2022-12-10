c4b9daea6829a7265744c0808587aa7f15cd24b3
maxihunter <maxi@hunter.com>
2022-12-10 07:34:48 +0300

    Added cuteclock class

--- cuteclock.cpp
+++ cuteclock.cpp
@@ -2,5 +2,60 @@
 
 cuteClock::cuteClock()
 {
-
 }
+
+int cuteClock::getLedMode() const {
+    return m_ledMode;
+};
+void cuteClock::setLedMode(int a_val) {
+    m_ledMode = a_val;
+};
+
+int cuteClock::getLedBrightness() const {
+    return m_ledBrightness;
+};
+void cuteClock::setLedBrightness(int a_val) {
+    m_ledBrightness = a_val;
+};
+
+int cuteClock::getHsvMode() const {
+    return m_hsvMode;
+};
+void cuteClock::setHsvMode(int a_val) {
+    m_hsvMode = a_val;
+};
+
+int cuteClock::getHourAlarmMode() const {
+    return m_hourAlarmMode;
+};
+void cuteClock::setHourAlarmMode(int a_val) {
+    m_hourAlarmMode = a_val;
+};
+
+int cuteClock::getHightHours() const {
+    return m_night_hours;
+};
+void cuteClock::setHightHours(int a_val) {
+    m_night_hours = a_val;
+};
+
+int cuteClock::getAlarmHours() const {
+    return m_alarm_hours;
+};
+void cuteClock::setAlarmHours(int a_val) {
+    m_alarm_hours = a_val;
+};
+
+int cuteClock::getAlarmMinutes() const {
+    return m_alarm_minutes;
+};
+void cuteClock::setAlarmMinutes(int a_val) {
+    m_alarm_minutes = a_val;
+};
+
+int cuteClock::getTimeFormat() const {
+    return m_time_format;
+};
+void cuteClock::setTimeFormat(int a_val) {
+    m_time_format = a_val;
+};
--- cuteclock.h
+++ cuteclock.h
@@ -1,13 +1,58 @@
 #ifndef CUTECLOCK_H
 #define CUTECLOCK_H
 
-
+/*ledAction[0] = stripRollingRainbow;
+  ledAction[1] = stripStaticRainbow;
+  ledAction[2] = stripCircleRainbow;
+  ledAction[3] = stripPulseRainbow;
+  ledAction[4] = stripArrowDots;
+  ledAction[5] = stripArrowOverlap;
+  ledAction[6] = stripArrowDotsSec;
+  ledAction[7] = stripArrowOverlapSec;
+  ledAction[8] = stripStaticCustom;
+  ledAction[9] = stripPulseCustom;
+*/
 class cuteClock
 {
 private:
-    int ledMode;
+    int m_ledMode;
+    int m_ledBrightness;
+    int m_hsvMode;
+    // XXXX XXXX - byte data
+    // 3210
+    // 01 - normal alarm mode (0 - off, 1 - on LED, 2 - sound alarm)
+    // 32 - sunrise mode (0 - off, othen prerun: 1 - 15 minute; 2 - 30 minutes; 3 - 60 minutes)
+    int m_hourAlarmMode;
+    int m_night_hours;
+    int m_alarm_hours;
+    int m_alarm_minutes;
+    int m_time_format;
 public:
     cuteClock();
+
+    int getLedMode() const;
+    void setLedMode(int a_val);
+
+    int getLedBrightness() const;
+    void setLedBrightness(int a_val);
+
+    int getHsvMode() const;
+    void setHsvMode(int a_val);
+
+    int getHourAlarmMode() const;
+    void setHourAlarmMode(int a_val);
+
+    int getHightHours() const;
+    void setHightHours(int a_val);
+
+    int getAlarmHours() const;
+    void setAlarmHours(int a_val);
+
+    int getAlarmMinutes() const;
+    void setAlarmMinutes(int a_val);
+
+    int getTimeFormat() const;
+    void setTimeFormat(int a_val);
 };
 
 #endif // CUTECLOCK_H
